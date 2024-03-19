38fe14ea669c5fcbc81b657ceae9b3d93eadb545
onyxvail <ons.hfi@gmail.com>
2024-03-19 16:09:38 +0000

    update-Navbar

--- Healixirs-shop/frontend/src/Components/Navbar/Navbar.jsx
+++ Healixirs-shop/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart from '../Assets/cart.png';
 
 export const Navbar = () => {
-  return (
-    <div className='navbar'>
-      Navbar
-      <div className="nav-logo">
-        <img src={logo} alt="" />
-      </div>
-      <ul className="nav-menu">
-        <li>Home</li>
-        <li>Shop</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div className="nav-cart">
-        <button> Login </button>
-        <img src={cart} alt="" />
-      </div>
-    </div>
-  );
+    const [menu, setMenu] = useState("Shop");
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p> Healixirs </p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => setMenu("Home")}>Home {menu === "Home" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("Shop")}>Shop {menu === "Shop" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("About")}>About {menu === "About" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("Contact")}>Contact {menu === "Contact" ? <hr /> : null}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button> Login </button>
+                <img src={cart} alt="" className="cart-image"  />
+                <div className="nav-cart-count">0</div>
+            </div>
+        </div>
+    );
 }
