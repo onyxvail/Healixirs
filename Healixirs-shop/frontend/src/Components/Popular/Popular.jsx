bf7a355d437f763c5d4cf262ab24de12a458b2fa
onyxvail <ons.hfi@gmail.com>
2024-03-20 22:13:35 +0000

    update

--- Healixirs-shop/frontend/src/Components/Popular/Popular.jsx
+++ Healixirs-shop/frontend/src/Components/Popular/Popular.jsx
@@ -7,20 +7,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 export const Popular = () => {
-  const repeatText = (text, count) => {
-    let repeatedText = '';
-    for (let i = 0; i < count; i++) {
-      repeatedText += text + ' '; // Add a space between each repetition
-    }
-    return repeatedText.trim(); // Trim to remove extra space at the end
-  };
-
-  const popularText = repeatText('Popular in Store', 8); // Repeat "Popular in Store" 8 times
-
   const settings = {
     dots: false,
     infinite: true,
-    speed: 50,
+    speed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -35,14 +25,18 @@ export const Popular = () => {
     <div className='popular'>
       <div className="popular-title-container">
         <Slider {...settings}>
-          <div className="popular-title-slide">{popularText}</div>
+          <div className="popular-title-slide">Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store</div>
         </Slider>
       </div>
       <hr/>   
       <div className="popular-item">
-        {data_product.map((item, i) => (
-          <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-        ))}
+        <Slider {...settings}> {/* Add slider for images */}
+          {data_product.map((item, i) => (
+            <div key={i} className="popular-item-slide">
+              <Item id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
