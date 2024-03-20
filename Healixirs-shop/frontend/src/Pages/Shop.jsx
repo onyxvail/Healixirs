52eb4f5e047e7fcd477e3ba45163b0707509052a
onyxvail <ons.hfi@gmail.com>
2024-03-20 11:01:39 +0000

    NewCollections-Added

--- Healixirs-shop/frontend/src/Pages/Shop.jsx
+++ Healixirs-shop/frontend/src/Pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Hero } from '../Components/Hero/Hero';
 import { Popular } from '../Components/Popular/Popular';
 import { Offer } from '../Components/Offers/Offer';
-
+import { NewCollections } from '../Components/NewCollections/NewCollections';
 const Shop = () => {
   return (
     <>
@@ -11,6 +11,7 @@ const Shop = () => {
       </Hero>
       <Popular />
       <Offer />
+      <NewCollections/>
     </>
   );
 };
