fe8a124e4230780420c631970e402f786e7bb234
onyxvail <ons.hfi@gmail.com>
2024-03-23 19:54:26 +0000

    RelatedProduct-update

--- Healixirs-shop/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ Healixirs-shop/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import data_product from '../Assets/data';
-import {Item} from '../Items/Item'; // Assuming Item is exported as default from 'Item.jsx'
+import {Item} from '../Items/Item';
 import './RelatedProducts.css';
 
 export const RelatedProducts = () => {
   return (
     <div className='relatedproducts flex flex-col items-center gap-10 h-90vh'>
       <h1 className="text-black text-5xl font-semibold">Related Products</h1>
-      <hr className="w-48 h-6 rounded-full bg-gray-700" />
+      <hr className="w-46 h-6 rounded-full bg-gray-700" />
 
       <div className="relatedproducts-item mt-10 flex gap-6">
         {data_product.map((item, i) => (
