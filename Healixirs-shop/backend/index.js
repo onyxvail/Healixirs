21bb18f7a4bb401d95777374c62fa53823be354d
onyxvail <ons.hfi@gmail.com>
2024-03-24 22:40:48 +0000

    second-endpoint{FIXED}

--- Healixirs-shop/backend/index.js
+++ Healixirs-shop/backend/index.js
@@ -7,49 +7,41 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
-
 app.use(express.json());
 app.use(cors());
 
 // DataBase Connection with MongoDB
-mongoose.connect("mongodb+srv://onyxvail:Onyx2121@cluster0.ueuajkx.mongodb.net/e-commerce")
+mongoose.connect("mongodb+srv://onyxvail:Onyx2121@cluster0.ueuajkx.mongodb.net/e-commerce");
 
 // API Creation
-
-app.get("/",(req,res)=>{
-    res.send("Express App is Running")
-})
+app.get("/", (req, res) => {
+    res.send("Express App is Running");
+});
 
 // Image Storage Engine
 const storage = multer.diskStorage({
     destination: "./upload/images",
-    filename:(req,file,cb)=>{
-             return cb(null,'${file.fieldname}_${Date.now()}${path.extname(file.originalname)}')
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
-})
+});
 
-const upload = multer({storage:storage})
+const upload = multer({storage: storage});
 
 // Creating Endpoint for Image Upload
+app.use('/images', express.static('upload/images'));
 
-app.use('/images' , express.static('upload/images'))
-
-app.post("/upload", upload.single('product'), (req,res) => {
+app.post("/upload", upload.single("product"), (req, res) => {
     res.json({
         success: 1,
         image_url: `http://localhost:${port}/images/${req.file.filename}`
     });
 });
 
-
-
-
-app.listen(port,(error)=>{
-    if (!error){
-        console.log('Server is running on port: '+port);
+app.listen(port, (error) => {
+    if (!error) {
+        console.log('Server is running on port: ' + port);
+    } else {
+        console.log('Error found: ' + error);
     }
-    else
-    {
-        console.log('Error found: '+error);
-    }
-})
+});
