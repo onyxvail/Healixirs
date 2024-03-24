8369c7203a5026aaa184cde74b23fcb05fe64dc7
onyxvail <ons.hfi@gmail.com>
2024-03-24 22:12:04 +0000

    second-endpoint{CREATED}

--- Healixirs-shop/backend/index.js
+++ Healixirs-shop/backend/index.js
@@ -30,6 +30,20 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
+// Creating Endpoint for Image Upload
+
+app.use('/images' , express.static('upload/images'))
+
+app.post("/upload",upload.single(product),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:'http://localhost:${port}/images/${req.file.filename}'
+    
+    })
+})
+
+
+
 
 app.listen(port,(error)=>{
     if (!error){
