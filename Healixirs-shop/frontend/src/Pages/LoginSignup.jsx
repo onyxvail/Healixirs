fc108c52ebd371f0ee0c27141372609c2a524123
onyxvail <ons.hfi@gmail.com>
2024-03-26 02:37:38 +0000

    linking login-frontend

--- Healixirs-shop/frontend/src/Pages/LoginSignup.jsx
+++ Healixirs-shop/frontend/src/Pages/LoginSignup.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginSignup = () => {
+  const [state] = useState("Login");
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-customNeon to-customBlack bg-fixed bg-cover">
-      <div className="w-full max-w-lg bg-customBlack shadow-xl rounded-lg p-8 transform transition-transform duration-500 hover:scale-105"> {/* Added transition and hover animation */}
-        <h1 className="text-customNeon text-4xl font-bold text-center mb-6">Sign Up</h1>
+      <div className="w-full max-w-lg bg-customBlack shadow-xl rounded-lg p-8 transform transition-transform duration-500 hover:scale-105">
+        <h1 className="text-customNeon text-4xl font-bold text-center mb-6">{state}</h1>
         <div className="flex justify-center">
           <form className="w-full space-y-6">
             <div className="relative">
-              <input
-                type="text"
-                placeholder="Your Name"
-                className="input border border-gray-300 px-4 py-3 rounded-lg w-full"
-              />
+              {state === "Sign Up" ? (
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="input border border-gray-300 px-4 py-3 rounded-lg w-full"
+                />
+              ) : (
+                <></>
+              )}
             </div>
             <div className="relative">
               <input
