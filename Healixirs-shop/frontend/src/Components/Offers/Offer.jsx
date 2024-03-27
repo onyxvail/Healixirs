e338022b0825813b38323ecc7cb518055e6d80f2
onyxvail <ons.hfi@gmail.com>
2024-03-27 15:27:38 +0000

    update-tailwind css

--- Healixirs-shop/frontend/src/Components/Offers/Offer.jsx
+++ Healixirs-shop/frontend/src/Components/Offers/Offer.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import exclusiveImage from '../Assets/exclusive_image.png';
 import Additional from '../Assets/additional.png';
 import { Link } from 'react-router-dom';
-import './Offer.css'; // Import CSS file for animations
+import './Offer.css';
 
 export const Offer = () => {
   return (
     <div className='flex items-center justify-center h-screen bg-customNeon'>
-      <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full md:w-4/5 lg:w-3/5 px-4"> {/* Adjusted container size here */}
-        <div className="md:w-1/2 order-2 md:order-1 animated fadeInLeft"> {/* Add animated class for animation */}
+      <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full md:w-4/5 lg:w-3/5 px-4 mt-4 mb-4"> {/* Adjusted container size and margin here */}
+        <div className="md:w-1/2 order-2 md:order-1 animated fadeInLeft">
           <img src={exclusiveImage} alt="" className="w-full h-auto hexagon-neon-shadow" />
         </div>
         <div className="md:w-1/2 order-1 md:order-2">
@@ -22,7 +22,7 @@ export const Offer = () => {
             <Link to="/offers" className="border border-white text-white py-2 px-4 rounded-full hover:bg-white hover:text-customNeon transition duration-300">Check Now</Link>
           </div>
         </div>
-        <div className="md:w-1/2 order-3 md:order-3 animated fadeInRight"> {/* Add animated class for animation */}
+        <div className="md:w-1/2 order-3 md:order-3 animated fadeInRight">
           <img src={Additional} alt="" className="w-full h-auto hexagon-neon-shadow" />
         </div>
       </div>
