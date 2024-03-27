3a9bc4f86c6e51eef82273240cb5f83eb054e72b
onyxvail <ons.hfi@gmail.com>
2024-03-27 03:45:48 +0000

    updated-offers

--- Healixirs-shop/frontend/src/Components/Offers/Offer.jsx
+++ Healixirs-shop/frontend/src/Components/Offers/Offer.jsx
@@ -2,28 +2,27 @@ import React from 'react';
 import exclusiveImage from '../Assets/exclusive_image.png';
 import Additional from '../Assets/additional.png';
 import { Link } from 'react-router-dom';
-import './Offer.css'; // Import CSS file for styling
 
 export const Offer = () => {
   return (
-    <div className='offer-container'>
-      <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full md:w-4/5 lg:w-3/5 px-4">
+    <div className='flex items-center justify-center h-screen bg-customNeon'>
+      <div className="flex flex-col md:flex-row md:items-center md:justify-center w-full md:w-4/5 lg:w-3/5 px-4"> {/* Adjusted container size here */}
         <div className="md:w-1/2 order-2 md:order-1">
-          <img src={exclusiveImage} alt="" className="w-full h-auto hexagon-neon-shadow image-animation" />
+          <img src={exclusiveImage} alt="" className="w-full h-auto hexagon-neon-shadow" />
         </div>
-        <div className="md:w-1/2 order-1 md:order-2 content-animation">
+        <div className="md:w-1/2 order-1 md:order-2">
           <div className="flex flex-col justify-center items-center md:items-start h-full">
-            <h1 className="text-3xl md:text-5xl font-bold text-center md:text-left mb-4 text-white glow-text">
+            <h1 className="text-3xl md:text-5xl font-bold text-center md:text-left mb-4 text-white">
               Exclusive Loot
               <br className="hidden md:block" />
               Deals for you.
             </h1>
-            <p className="text-lg text-center md:text-left mb-4 text-customBlack glow-text">FOR THE HORDE.</p>
-            <Link to="/womens" className="border border-white text-white py-2 px-4 rounded-full hover:bg-white hover:text-customNeon transition duration-300">Check Now</Link>
+            <p className="text-lg text-center md:text-left mb-4 text-customBlack">Only for the hoarders of the epic loot.</p>
+            <Link to="/offers" className="border border-white text-white py-2 px-4 rounded-full hover:bg-white hover:text-customNeon transition duration-300">Check Now</Link>
           </div>
         </div>
         <div className="md:w-1/2 order-3 md:order-3">
-          <img src={Additional} alt="" className="w-full h-auto hexagon-neon-shadow image-animation" />
+          <img src={Additional} alt="" className="w-full h-auto hexagon-neon-shadow" />
         </div>
       </div>
     </div>
--- Healixirs-shop/frontend/src/Components/Popular/Popular.jsx
+++ Healixirs-shop/frontend/src/Components/Popular/Popular.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './Popular.css';
+import './Popular.css'; // Importing CSS may not be necessary if styles are defined in Tailwind
 import data_product from '../Assets/data.js';
 import { Item } from '../Items/Item';
 import Slider from 'react-slick';
@@ -25,14 +24,19 @@ export const Popular = () => {
     <div className='popular'>
       <div className="popular-title-container">
         <Slider {...settings}>
-          <div className="popular-title-slide"> Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store PopularPopular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store</div>
+          <div className="popular-title-slide flex items-center space-x-2 overflow-hidden text-customBlue text-xl bg-customGray">
+            {/* Custom classes added for styling */}
+            Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store Popular Popular in Store Popular in Store PopularPopular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store Popular in Store
+          </div>
         </Slider>
       </div>
-      <hr/>   
-      <div className="popular-item">
+      <hr/>
+      <div className="popular-item overflow-hidden">
+        {/* Added overflow-hidden class */}
         <Slider {...settings}>
           {data_product.map((item, i) => (
-            <div key={i} className="popular-item-slide">
+            <div key={i} className="popular-item-slide inline-block">
+              {/* Removed custom animation classes as Tailwind does not support animations */}
               <Item id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
             </div>
           ))}
